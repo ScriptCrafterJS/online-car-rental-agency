a336a1bfb80f06326bff402c54fb5f3400958d5b
Motasem Ali <motasemali0000@gmail.com>
2024-06-12 09:04:39 +0300

    build login page and if the user is on the system he could login

--- app/login.php
+++ app/login.php
@@ -1,3 +1,33 @@
+<?php 
+require_once('./db.php.inc');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+     //connect to the database
+     $pdo = db_connect();
+
+     $sql = $pdo->prepare("SELECT * FROM users WHERE password = :password AND username = :username");
+     $sql->bindParam(':password', $_POST['password']);
+     $sql->bindParam(':username', $_POST['username']);
+     $sql->execute();
+    $user = $sql->fetch();
+
+    if($user){
+        if($user['isManager']){
+            //go to the manager page
+            header('Location: manager.php');
+            exit();
+        }else{
+            //go to the customer page
+            header('Location: customer.php');
+            exit();
+        }
+    }
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +38,24 @@
 </head>
 
 <body>
-    hi here you will login
+    <?php 
+        echo displayHead();
+    ?>
+    <fieldset>
+        <legend>Login</legend>
+        <form method="POST" action="">
+            <label>Username:</label><br>
+            <input type="text" name="username" required minlength="6" maxlength="13"><br><br>
+
+            <label>Password:</label><br>
+            <input type="password" name="password" required minlength="8" maxlength="12"><br><br>
+
+            <button type="submit">Login</button>
+        </form>
+    </fieldset>
+    <?php 
+    echo displayFooter();
+    ?>
 </body>
 
 </html>
